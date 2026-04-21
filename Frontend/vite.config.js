ab695a1789b701c0e9466d7f7c943dbf0431399d
Rasmus <rasmus.waleij@gmail.com>
2026-04-21 13:36:50 +0200

    .env updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  base: '/personalportalen/',
   server: {
     https: true,
     port: 5173,
