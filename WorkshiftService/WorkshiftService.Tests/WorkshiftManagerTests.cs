b4507a8d62f49f6f0654ed00e9561210d57dd5f7
Rasmus <rasmus.waleij@gmail.com>
2026-08-21 20:52:20 +0200

    Deploy updated

--- WorkshiftService/WorkshiftService.Tests/WorkshiftManagerTests.cs
+++ WorkshiftService/WorkshiftService.Tests/WorkshiftManagerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dtos;
 using Application.Interfaces;
 using Application.Services.Managers;
+using Domain.Entities;
 using Moq;
 
 namespace WorkshiftService.Tests;
@@ -29,5 +30,35 @@ public class WorkshiftManagerTests
         var result = await manager.CreateAsync(form, "user-1");
 
         Assert.True(result.Succeeded);
+    }
+
+    [Fact]
+    public async Task CreateAsync_ShouldAddAndSaveWorkshift()
+    {
+        var repository = new Mock<IWorkshiftRepository>();
+        var bookingClient = new Mock<IBookingClient>();
+
+        var manager = new WorkshiftManager(
+            repository.Object,
+            bookingClient.Object);
+
+        var form = new WorkshiftRegistrationForm
+        {
+            Area = "Test",
+            Level = "Junior",
+            Starttime = DateTime.UtcNow,
+            Endtime = DateTime.UtcNow.AddHours(8),
+            EmployeeId = "employee-1"
+        };
+
+        await manager.CreateAsync(form, "user-1");
+
+        repository.Verify(
+            x => x.AddAsync(It.IsAny<WorkshiftEntity>()),
+            Times.Once);
+
+        repository.Verify(
+            x => x.SaveAsync(),
+            Times.Once);
     }
 }
